afa65c2897805aadbac84e96cf739c79a1a8816a
Kira Mironova <kiracabezas@gmail.com>
2021-05-11 11:28:42 +0100

    Commit final

--- src/main/java/com/mycompany/reservacoches/Cliente.java
+++ src/main/java/com/mycompany/reservacoches/Cliente.java
@@ -5,6 +5,6 @@ import java.util.List;
 public class Cliente {
     private String codigo, nombre, dni, direccion, telefono;
     private Cliente aval, avaladoPor;
-    private List<Reservacion> reservas;
+    private List<Reservacion> reservasDelCliente;
     
 }
--- src/main/java/com/mycompany/reservacoches/ReservacionDetalle.java
+++ src/main/java/com/mycompany/reservacoches/ReservacionDetalle.java
@@ -1,7 +1,6 @@
 package com.mycompany.reservacoches;
 
 import java.util.Date;
-import java.util.List;
 
 public class ReservacionDetalle {
     private Date fechaInicio, fechaFinal;
